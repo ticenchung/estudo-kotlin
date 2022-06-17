0be3fb54625b21bc04f426c3a5edc95044b46b2c
Chi Simg Chung <lzcg@gft.com>
2022-06-17 11:19:28 -0300

    add product-service project

---
+++ product/src/test/kotlin/br/com/luiz/product/ProductApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.luiz.product
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
